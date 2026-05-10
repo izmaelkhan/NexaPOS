2fefb06b0c6e8ae70a76a039490292360e1dc51f
ismail khan <ism_kh13@yahoo.com>
2026-05-10 10:54:05 +0500

    Backend Founcation and Architecture setup

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
